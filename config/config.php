6cbac9bf376b59f2b9c8f5ea2a16c1eab4c234b3
EL-MOURABITsaber <85076651+EL-MOURABITsaber@users.noreply.github.com>
2023-10-09 16:54:55 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,20 +1,31 @@
 <?php
+
+
 return [
+
+
     'available_modules' => [
 
         'users_managment' => [
-            'name' => 'Module One',
+            'name' => 'Posts', // shouldn't have spaces
             'description' => 'Description of Module One.',
-            'link'=>'https://github.com/barryvdh/laravel-debugbar.git',
-            'enabled' => false,
+            'category'=>'user auth',
+            'link'=>'https://github.com/EL-MOURABITsaber/first-module',
+            'downloaded'=>true,
+            'enabled' => true,
+           
+
         ],
         
         'module_two' => [
             'name' => 'Module Two',
             'description' => 'Description of Module Two.',
             'link'=>'https://github.com/barryvdh/laravel-ide-helper',
+            'category'=>'user auth',
+            'downloaded'=>false,
             'enabled' => false,
         ],
+        
 
     ],  
 ];
